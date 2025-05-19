86ccd03419be2b1af03f76520c40ce0d819c4ab9
AndreJorge01 <andrejorge865@gmail.com>
2025-05-19 05:11:16 -0300

    first commit

---
+++ variaveis.js
@@ -0,0 +1,31 @@
+var nome1 = "André";
+var idade1 = "22";
+var possuiFaculdade = "false";
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+var sobrenome = "Jorge",
+  cidade = "São Paulo";
+
+var semDefinir;
+
+var comida1;
+comida1 = "Pizza";
+
+//const time = 'dallas';
+//time = 'patriots';
+//console.log(time)
+
+var nome = "André";
+var idade = "22";
+var comidaFavorita;
+comidaFavorita = "Pizza";
+var carro,
+ moto,
+ casa,
+ pais,
+ estado;
+
+console.log(nome, idade, comidaFavorita);
