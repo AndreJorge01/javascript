cfe380e259c4bae3ac71f732d7d97ecd8126971d
AndreJorge01 <andrejorge865@gmail.com>
2025-06-03 06:50:07 -0300

    objeto

--- objetos/objetos.js
+++ objetos/objetos.js
@@ -140,3 +140,15 @@ var carro = {
 carro.preco = 3000;
 // Crie um objeto de um cachorro que represente um labrador,
 // preto com 10 anos, que late ao ver um homem
+var cachorro = {
+  raca: "labrador",
+  cor: "preto",
+  idade: 10,
+  latir(pessoa) {
+    if (pessoa === "homen") {
+      return "latir";
+    } else {
+      return "nada";
+    }
+  },
+};
