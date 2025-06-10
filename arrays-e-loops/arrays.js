9ec19da3467c7f2faa288e67d9b97055a2cab046
AndreJorge01 <andrejorge865@gmail.com>
2025-06-10 06:16:04 -0300

    array

--- arrays-e-loops/arrays.js
+++ arrays-e-loops/arrays.js
@@ -1,19 +1,19 @@
 //Array
 //É um grupo de valores geralmente relacionados. Servem para guardarmos diferentes valores em uma única variável.
 
-var videoGames = ['Switch', 'PS4', 'XBox'];
+var videoGames = ["Switch", "PS4", "XBox"];
 
-videoGames[0] // Switch
-videoGames[2] // Xbox
+videoGames[0]; // Switch
+videoGames[2]; // Xbox
 
 //Acesse um elemento da array utilizando [n]
 
 //Métodos e Propriedades de uma Array
 
-var videoGames = ['Switch', 'PS4', 'XBox'];
+var videoGames = ["Switch", "PS4", "XBox"];
 
 videoGames.pop(); // Remove o último item e retorna ele
-videoGames.push('3DS'); // Adiciona ao final da array
+videoGames.push("3DS"); // Adiciona ao final da array
 videoGames.length; // 3
 
 //Existem diversos outros métodos, como map, reduce, forEach e mais que veremos mais à frente
@@ -28,7 +28,6 @@ for (var numero = 0; numero < 10; numero++) {
 
 //O for loop possui 3 partes, início, condição e incremento
 
-
 //While Loop
 
 var i = 0;
@@ -41,7 +40,7 @@ while (i < 10) {
 //O for loop é o mais comum
 
 //Arrays e Loops
-var videoGames = ['Switch', 'PS4', 'XBox', '3DS'];
+var videoGames = ["Switch", "PS4", "XBox", "3DS"];
 for (var i = 0; i < videoGames.length; i++) {
   console.log(videoGames[i]);
 }
@@ -50,20 +49,19 @@ for (var i = 0; i < videoGames.length; i++) {
 //Break
 //O loop irá parar caso encontro e palavra break
 
-var videoGames = ['Switch', 'PS4', 'XBox', '3DS'];
+var videoGames = ["Switch", "PS4", "XBox", "3DS"];
 for (var i = 0; i < videoGames.length; i++) {
   console.log(videoGames[i]);
-  if(videoGames[i] === 'PS4') {
+  if (videoGames[i] === "PS4") {
     break;
   }
 }
 
-
 //forEach
 //forEach é um método que executa uma função para cada item da Array. É uma forma mais simples de utilizarmos um loop com arrays (ou array-like)
 
-var videoGames = ['Switch', 'PS4', 'XBox', '3DS'];
-videoGames.forEach(function(item) {
+var videoGames = ["Switch", "PS4", "XBox", "3DS"];
+videoGames.forEach(function (item) {
   console.log(item);
 });
 // O argumento item será atribuído dinamicamente
@@ -74,7 +72,7 @@ videoGames.forEach(function(item) {
 
 var numero = 0;
 var maximo = 50;
-for(;numero < maximo;) {
+for (; numero < maximo; ) {
   console.log(numero);
   numero++;
 }
@@ -85,13 +83,23 @@ for(;numero < maximo;) {
 
 // Crie uma array com os anos que o Brasil ganhou a copa
 // 1959, 1962, 1970, 1994, 2002
+var Copas = ["1959", "1962", "1970", "1994", "2002"];
 
 // Interaja com a array utilizando um loop, para mostrar
 // no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
+for (var i = 0; i < Copas.length; i++) {
+  console.log(`O brasil foi campeão na copa de ${Copas[i]}`);
+}
 
 // Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
-var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância']
+var frutas = ["Banana", "Maçã", "Pera", "Uva", "Melância"];
+for (item = 0; item < frutas.length; item++) {
+  console.log(frutas[item]);
+  if (frutas[item] === "Pera") {
+    break;
+  }
+}
 
 // Coloque a última fruta da array acima em uma variável,
 // sem remover a mesma da array.
-
+var ultimaFruta = frutas[frutas.length - 1];
